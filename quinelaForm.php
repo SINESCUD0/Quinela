eaa1a15e889fa1dcd0d3a83cd8097ad13b55ba16
Iván García <114983932+SINESCUD0@users.noreply.github.com>
2023-10-17 09:27:38 +0200

    Update quinelaForm.php

--- quinelaForm.php
+++ quinelaForm.php
@@ -53,14 +53,6 @@ $equipos = array(
 	"Andorra",
 	"Racing"
 );
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-	//Guarda los resultados que hemos introducido en la tabla, pero no consigo que guarden los partidos que estan relacionados a los resultados
-	for($i = 0; $i < 14; $i++){
-		$r = $i + 1;
-		$columna[$i][2] = ($_POST["resultado".$r]);
-	}
-	var_dump($columna);
-}
 //Genero los partidos de una jornada aleatoriamente
 if ($cantidad_numeros > ($max - $min + 1)) {
 	echo "No se pueden generar más números únicos que la longitud del rango.";
@@ -75,19 +67,15 @@ if ($cantidad_numeros > ($max - $min + 1)) {
 			$jornada[] = array($equipos[$numero_aleatorio], $equipos[$numero_aleatorio2]);
 	}
 }
-/*$resultadosLength = strlen($resultados);
-for($col = 0; $col < 8; $col++){
-	for($fi = 0; $fi < 14; $fi++){
-		$quinela[$col][$fi] = 0;
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	//Guarda los resultados que hemos introducido en la tabla, pero no consigo que guarden los partidos que estan relacionados a los resultados
+	for($i = 0; $i < 14; $i++){
+		$r = $i + 1;
+		$columna[$i][0] = test_input($_POST["partido".$r]);
+		$columna[$i][1] = ($_POST["resultado".$r]);
 	}
+	var_dump($columna);
 }
-for($col = 0; $col < rand(1,8); $col++){
-	for($fi = 0; $fi < 14; $fi++){
-		$quinela[$col][$fi] = $resultados[random_int(0, $resultadosLength - 1)];
-	}
-}*/
-//$tabla = "<table>";
-
 
 /*for($jornadas = 0; $jornadas < 56; $jornadas++){
 	if ($cantidad_numeros > ($max - $min + 1)) {
@@ -118,22 +106,12 @@ for($col = 0; $col < rand(1,8); $col++){
 
 var_dump($numeros_generados);
 var_dump($jornada);
-/*foreach($jornada as $elemento){
-	$tabla .= "<td>".$elemento[0]."<br/> vs <br/>".$elemento[1]."</td>";
-}*/
 
-/*foreach($quinela as $columna){
-	$tabla .= "<tr>";
-	foreach($columna as $fila){
-		$tabla .= "<td>".$fila."</td>";
-	}
-	$tabla .= "</tr>";
-}*/
 //Creo la tabla donde digo los partidos y luego la columna con los resultados
 $tabla2 = "
 <table>
 	<tr>
-		<td>".$jornada[0][0]." vs ".$jornada[0][1]."</td>
+		<td><input type='text' name='partido1' value='".$jornada[0][0]." vs ".$jornada[0][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado1' value='1' />1
 			<input type='radio' name='resultado1' value='X' />X
@@ -141,7 +119,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[1][0]." vs ".$jornada[1][1]."</td>
+		<td><input type='text' name='partido2' value='".$jornada[1][0]." vs ".$jornada[1][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado2' value='1' />1
 			<input type='radio' name='resultado2' value='X' />X
@@ -149,7 +127,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[2][0]." vs ".$jornada[2][1]."</td>
+		<td><input type='text' name='partido3' value='".$jornada[2][0]." vs ".$jornada[2][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado3' value='1' />1
 			<input type='radio' name='resultado3' value='X' />X
@@ -157,7 +135,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[3][0]." vs ".$jornada[3][1]."</td>
+		<td><input type='text' name='partido4' value='".$jornada[3][0]." vs ".$jornada[3][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado4' value='1' />1
 			<input type='radio' name='resultado4' value='X' />X
@@ -165,7 +143,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[4][0]." vs ".$jornada[4][1]."</td>
+		<td><input type='text' name='partido5' value='".$jornada[4][0]." vs ".$jornada[4][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado5' value='1' />1
 			<input type='radio' name='resultado5' value='X' />X
@@ -173,7 +151,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[5][0]." vs ".$jornada[5][1]."</td>
+		<td><input type='text' name='partido6' value='".$jornada[5][0]." vs ".$jornada[5][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado6' value='1' />1
 			<input type='radio' name='resultado6' value='X' />X
@@ -181,7 +159,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[6][0]." vs ".$jornada[6][1]."</td>
+		<td><input type='text' name='partido7' value='".$jornada[6][0]." vs ".$jornada[6][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado7' value='1' />1
 			<input type='radio' name='resultado7' value='X' />X
@@ -189,7 +167,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[7][0]." vs ".$jornada[7][1]."</td>
+		<td><input type='text' name='partido8' value='".$jornada[7][0]." vs ".$jornada[7][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado8' value='1' />1
 			<input type='radio' name='resultado8' value='X' />X
@@ -197,7 +175,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[8][0]." vs ".$jornada[8][1]."</td>
+		<td><input type='text' name='partido9' value='".$jornada[8][0]." vs ".$jornada[8][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado9' value='1' />1
 			<input type='radio' name='resultado9' value='X' />X
@@ -205,7 +183,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[9][0]." vs ".$jornada[9][1]."</td>
+		<td><input type='text' name='partido10' value='".$jornada[9][0]." vs ".$jornada[9][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado10' value='1' />1
 			<input type='radio' name='resultado10' value='X' />X
@@ -213,7 +191,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[10][0]." vs ".$jornada[10][1]."</td>
+		<td><input type='text' name='partido11' value='".$jornada[10][0]." vs ".$jornada[10][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado11' value='1' />1
 			<input type='radio' name='resultado11' value='X' />X
@@ -221,7 +199,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[11][0]." vs ".$jornada[11][1]."</td>
+		<td><input type='text' name='partido12' value='".$jornada[11][0]." vs ".$jornada[11][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado12' value='1' />1
 			<input type='radio' name='resultado12' value='X' />X
@@ -229,7 +207,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[12][0]." vs ".$jornada[12][1]."</td>
+		<td><input type='text' name='partido13' value='".$jornada[12][0]." vs ".$jornada[12][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado13' value='1' />1
 			<input type='radio' name='resultado13' value='X' />X
@@ -237,7 +215,7 @@ $tabla2 = "
 		</td>
 	</tr>
 	<tr>
-		<td>".$jornada[13][0]." vs ".$jornada[13][1]."</td>
+		<td><input type='text' name='partido14' value='".$jornada[13][0]." vs ".$jornada[13][1]."' readonly /></td>
 		<td>
 			<input type='radio' name='resultado14' value='1' />1
 			<input type='radio' name='resultado14' value='X' />X
@@ -247,9 +225,13 @@ $tabla2 = "
 </table>
 <br/>";
 
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
 
-//var_dump($quinela);
-//echo $tabla."</table>";
 echo $tabla2;
 ?>
 <input type='submit' name='Enviar'/>
